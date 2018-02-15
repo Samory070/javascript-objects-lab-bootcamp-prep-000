8e4aa51d06f32906758d00fc39bc1ba0b21f0ee5
samory harris <samory070@gmail.com>
2018-02-15 18:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var obj = Object.assign({}, object)
+  var obj = Object.assign({}, object, key)
    delete obj.key
   return obj, object
 }
