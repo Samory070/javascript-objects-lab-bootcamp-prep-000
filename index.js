31aa92b609e0a01a6dd0be1d47bd1abd62e595d4
samory harris <samory070@gmail.com>
2018-02-15 18:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  var obj = Object.assign({}, object, {[key]: value})
+  // object[key] = value
+  return obj
 }
 
 function deleteFromObjectByKey(object, key) {
